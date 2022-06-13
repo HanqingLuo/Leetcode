3bc809966e070d21986bb95952deeb8329c2ceef
Hanqing Luo <kukuman520@gmail.com>
2022-06-13 11:03:08 -0400

    Update 98. Validate Binary Search Tree.py

--- Leetcode Blind 75/98. Validate Binary Search Tree.py
+++ Leetcode Blind 75/98. Validate Binary Search Tree.py
@@ -5,12 +5,14 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool: 
-        if not p and not q: 
-            return True
-        elif not p or not q: 
-            return False
-        elif p.val != q.val: 
-            return False
-        else: 
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def validate(node, low=-math.inf, high=math.inf):
+            if not node: return True
+            if node and node.val >= high or node.val <= low:return False
+            return validate(node.left,low,node.val) and validate(node.right,node.val,high)
+        return validate(root)
+            
+            
+            
+            
+        
