353f27acff79ef9937cac49167304a2e4738de0f
Hanqing Luo <kukuman520@gmail.com>
2022-06-15 10:38:07 -0400

    Update 572. Subtree of Another Tree.py

--- Leetcode Blind 75/572. Subtree of Another Tree.py
+++ Leetcode Blind 75/572. Subtree of Another Tree.py
@@ -1,18 +1,22 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+# Definition for a binary tree root.
+# class Treeroot:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        node = root
-        while True:
-            if p.val > node.val and q.val > node.val:
-                node = node.right
-            elif p.val < node.val and q.val < node.val:
-                node = node.left
-            else: 
-                return node
-            
+    def isSubtree(self, root: TreeNode, subRoot: TreeNode) -> bool:
+        if not subRoot: return True
+        if not root: return False
+
+        if self.isSameTree(root, subRoot): return True
+        return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
+        
+
+    # 100. 相同的树
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q: 
+            return True
+        if p and q and p.val == q.val: 
+            return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+        return False
